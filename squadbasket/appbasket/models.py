945eb1c54d6600ee02ee9a868f3813beef74fe7c
Adrian Rojas <adriian12@users.noreply.github.com>
2018-06-07 17:09:31 +0200

    plantillas actualizadas
    añadido código django a las plantillas

--- squadbasket/appbasket/models.py
+++ squadbasket/appbasket/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.utils.encoding import smart_unicode
 
 # class Home(models.Model):
 #     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -10,7 +10,9 @@ from django.utils import timezone
 #     imagen = models.ImageField(upload_to='uploads/logo')
 
 
-class Noticias(models.Model):
+
+
+class Noticia(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     titulo = models.CharField(max_length=50)
     texto = models.TextField(max_length=400)
@@ -21,20 +23,38 @@ class Noticias(models.Model):
         self.publicada = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.titulo
+    def __unicode__(self):
+        return (u"%s" % self.titulo)
 
 class Slider(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.TextField(max_length=200)
-    imagen = models.ImageField(upload_to='media/uploads/slider')
+    imagen = models.ImageField(upload_to='slides')
 
-    def __str__(self):
-        return self.titulo
+    def __unicode__(self):
+        return (u"%s" % self.titulo)
 
-class Columnas(models.Model):
+class Columna(models.Model):
     col_titulo = models.CharField(max_length=50)
     col_texto = models.TextField(max_length=200)
 
-    def __str__(self):
-        return self.col_titulo
+    def __unicode__(self):
+        return (u"%s" % self.col_titulo)
+
+class Reciente(models.Model):
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=40)
+    texto = models.TextField(max_length=300)
+    imagen = models.ImageField(upload_to='noticias')
+
+    def __unicode__(self):
+        return (u"%s" % self.titulo)
+
+class Directiva(models.Model):
+    nombre = models.CharField(max_length=30)
+    puesto = models.CharField(max_length=30)
+    imagen = models.ImageField(upload_to='directiva')
+    tabla = models.TextField(null=True, blank=True, default='')
+
+    def __unicode__(self):
+        return (u"%s" % self.nombre)
